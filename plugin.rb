2a4853541f25ddb7a9b769322cf2bd1c37a6fe51
Sekhat Temporus <sekhat@manalightgames.com>
2015-06-24 14:20:32 +0100

    Add: plugin meta information

--- plugin.rb
+++ plugin.rb
@@ -1,1 +1,7 @@
-register_asset("javascript/yuml_dialect.js")
+# name: discourse-yuml
+# about: Adds the ability to add yUML diagrams to posts.
+# version: 0.0.1
+# authors: Sekhat Temporus
+# url: https://github.com/sekhat/discourse-yuml
+
+register_asset "javascript/yuml_dialect.js"
