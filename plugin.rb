f51d799028f48fb89d2b7fe73c8e27e5a37a0548
Sekhat Temporus <sekhat@manalightgames.com>
2017-08-09 20:20:53 +0100

    Update version

--- plugin.rb
+++ plugin.rb
@@ -1,5 +1,5 @@
 # name: discourse-yuml
 # about: Adds the ability to add yUML diagrams to posts.
-# version: 0.2.0
+# version: 0.2.1
 # authors: Sekhat Temporus
 # url: https://github.com/sekhat/discourse-yuml
